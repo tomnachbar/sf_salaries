1ee173027ac4dbd67443b523ae938af43d7045c8
Eliton Mauro Nachbar <140862399+tomnachbar@users.noreply.github.com>
2024-02-05 15:52:10 -0300

    Update 💼_JobTitle.py
    adjust year filter

--- pages/💼_JobTitle.py
+++ pages/💼_JobTitle.py
@@ -80,7 +80,7 @@ with st.sidebar:
 st.sidebar.markdown('**YEAR FILTER:**')   
 year = st.sidebar.slider('Choose the year:', min_value=2011,
                          max_value=2014,
-                         value=int(df1['Year'].min()),  # Define o valor inicial como o ano mínimo do DataFrame
+                         value=int(df1['Year'].max()),  
                          step=1)
 
 # FILTER of JOB
